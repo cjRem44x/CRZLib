96a03150c5670e8d32fcfdfa2d8ba074382f2c24
Carrick <remillardc15@gmail.com>
2025-01-31 11:45:25 -0500

    feat!: liberr reporting

--- main.zig
+++ main.zig
@@ -1,3 +1,9 @@
 const cz = @import("czlib.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var buf: [10]u8 = undefined;
+    const name = try cz.cin(&buf, "enter name: ");
+    cz.print("hello {s}\n", .{name});
+
+    cz.liberr("testing");
+}
